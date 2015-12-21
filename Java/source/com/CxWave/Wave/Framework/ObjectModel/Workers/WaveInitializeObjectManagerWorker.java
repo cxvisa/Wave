6409c778d240e2638cad4a52b8c86edfe40e0568
Vidyasagara Guntaka <sagar@cxwave.com>
2015-12-21 13:44:02 -0800

    More basic boot related messages

---
+++ Java/source/com/CxWave/Wave/Framework/ObjectModel/Workers/WaveInitializeObjectManagerWorker.java
@@ -0,0 +1,24 @@
+/*************************************************************************************************************************
+ * Copyright (C) 2015-2015 Vidyasagara Guntaka & CxWave, Inc * All rights reserved. * Author : Vidyasagara Reddy Guntaka *
+ *************************************************************************************************************************/
+
+package com.CxWave.Wave.Framework.ObjectModel.Workers;
+
+import com.CxWave.Wave.Framework.ObjectModel.WaveObjectManager;
+import com.CxWave.Wave.Framework.ObjectModel.WaveWorker;
+import com.CxWave.Wave.Framework.ObjectModel.WaveWorkerPriority;
+import com.CxWave.Wave.Framework.ObjectModel.Annotations.Cardinality;
+import com.CxWave.Wave.Framework.ObjectModel.Annotations.OwnerOM;
+import com.CxWave.Wave.Framework.ObjectModel.Annotations.Priority;
+
+@OwnerOM (om = WaveObjectManager.class)
+@Cardinality (1)
+@Priority (WaveWorkerPriority.WAVE_WORKER_PRIORITY_0)
+public class WaveInitializeObjectManagerWorker extends WaveWorker
+{
+    public WaveInitializeObjectManagerWorker (final WaveObjectManager waveObjectManager)
+    {
+        super (waveObjectManager);
+    }
+
+}
