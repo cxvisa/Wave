0f139d36a16e66bfbc72ccfee541bf1f31d9c6e4
Vidyasagara Guntaka <sagar@cxwave.com>
2016-05-05 15:57:53 -0700

    light pulse message base class

---
+++ C++/source/Framework/Messaging/LightHouse/LightPulse.cpp
@@ -0,0 +1,85 @@
+/***************************************************************************
+ *   Copyright (C) 20016 Vidyasagara Guntaka                               *
+ *   All rights reserved.                                                  *
+ *   Author : Vidyasagara Reddy Guntaka                                    *
+ ***************************************************************************/
+
+#include "Framework/Messaging/LightHouse/LightPulse.h"
+#include "Framework/Attributes/Attributes.h"
+#include "Framework/Attributes/AttributeUUID.h"
+
+namespace WaveNs
+{
+
+LightPulse::LightPulse (const string &name)
+    : m_name (name),
+      m_senderLightHousePort (0)
+{
+}
+
+LightPulse::~LightPulse ()
+{
+}
+
+string LightPulse::getName () const
+{
+    return m_name;
+}
+
+void LightPulse::setName (const string& name)
+{
+    m_name = name;
+}
+
+string LightPulse::getSenderIpAddressFromClientPerspective () const
+{
+    return m_senderIpAddressFromClientPerspective;
+}
+
+void LightPulse::setSenderIpAddressFromClientPerspective (const string &senderIpAddressFromClientPerspective)
+{
+    m_senderIpAddressFromClientPerspective = senderIpAddressFromClientPerspective;
+}
+
+Uuid LightPulse::getSenderLightHouseId () const
+{
+    return m_senderLightHouseId;
+}
+
+void LightPulse::setSenderLightHouseId (const Uuid &senderLightHouseId)
+{
+    m_senderLightHouseId = senderLightHouseId;
+}
+
+string LightPulse::getSenderLightHouseIpAddress () const
+{
+    return m_senderLightHouseIpAddress;
+}
+
+void LightPulse::setSenderLightHouseIpAddress (const string &senderLightHouseIpAddress)
+{
+    m_senderLightHouseIpAddress = senderLightHouseIpAddress;
+}
+
+SI32 LightPulse::getSenderLightHousePort () const
+{
+    return m_senderLightHousePort;
+}
+
+void LightPulse::setSenderLightHousePort (const SI32 &senderLightHousePort)
+{
+    m_senderLightHousePort = senderLightHousePort;
+}
+
+void LightPulse::setupAttributesForSerialization ()
+{
+    SerializableObject::setupAttributesForSerialization ();
+
+    addSerializableAttribute (new AttributeString (&m_name,                                 "name"));
+    addSerializableAttribute (new AttributeUUID   (&m_senderLightHouseId,                   "senderLightHouseId"));
+    addSerializableAttribute (new AttributeString (&m_senderLightHouseIpAddress,            "senderLightHouseIpAddress"));
+    addSerializableAttribute (new AttributeSI32   (&m_senderLightHousePort,                 "senderLightHousePort"));
+    addSerializableAttribute (new AttributeString (&m_senderIpAddressFromClientPerspective, "senderIpAddressFromClientPerspective"));
+}
+
+}
---
+++ C++/source/Framework/Messaging/LightHouse/LightPulse.h
@@ -0,0 +1,56 @@
+/***************************************************************************
+ *   Copyright (C) 20016 Vidyasagara Guntaka                               *
+ *   All rights reserved.                                                  *
+ *   Author : Vidyasagara Reddy Guntaka                                    *
+ ***************************************************************************/
+
+#ifndef LIGHTPULSE_H
+#define LIGHTPULSE_H
+
+#include "Framework/ObjectModel/SerializableObject.h"
+#include "Framework/Types/Uuid.h"
+
+namespace WaveNs
+{
+
+class LightPulse : public SerializableObject
+{
+    private :
+        void setupAttributesForSerialization ();
+
+    protected :
+    public :
+                        LightPulse                              (const string &Name);
+        virtual        ~LightPulse                              ();
+
+                string  getName                                 () const;
+                void    setName                                 (const string &name);
+
+                Uuid    getSenderLightHouseId                   () const;
+                void    setSenderLightHouseId                   (const Uuid &senderLightHouseId);
+
+                string  getSenderLightHouseIpAddress            () const;
+                void    setSenderLightHouseIpAddress            (const string &senderLightHouseIpAddress);
+
+                SI32    getSenderLightHousePort                 () const;
+                void    setSenderLightHousePort                 (const SI32 &port);
+
+                string  getSenderIpAddressFromClientPerspective () const;
+                void    setSenderIpAddressFromClientPerspective (const string &senderIpAddressFromClientPerspective);
+
+        // Now the data members
+
+    private :
+        string m_name;
+        Uuid   m_senderLightHouseId;
+        string m_senderLightHouseIpAddress;
+        SI32   m_senderLightHousePort;
+        string m_senderIpAddressFromClientPerspective;
+
+    protected :
+    public :
+};
+
+}
+
+#endif // LIGHTPULSE_H
